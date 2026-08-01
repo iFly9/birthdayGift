aabe5ee3459244b1ad4f87a7b2c8ece74d457309
Kauasalya <kausalya.546@gmail.com>
2026-08-01 19:49:18 +0530

    add

--- src/components/Gallery/MemoryCard.jsx
+++ src/components/Gallery/MemoryCard.jsx
@@ -46,12 +46,16 @@ export default function MemoryCard({
 
                         exit={{
                             scale: .8,
-                            opacity: 0
+                            opacity: 0,
+                             transition: {
+        duration: 0.15
+    }
                         }}
 
                         transition={{
                             type: "spring",
-                            stiffness: 120
+    stiffness: 180,
+    damping: 20
                         }}
 
                     >
