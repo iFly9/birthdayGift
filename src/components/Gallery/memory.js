2fbe4af924b8330dfa53efe6af0cb5b5dbc9eaa4
Kauasalya <kausalya.546@gmail.com>
2026-09-10 10:56:59 +0530

    add

--- src/components/Gallery/memory.js
+++ src/components/Gallery/memory.js
@@ -26,69 +26,133 @@ export const memories = [
     image: img1,
     song: song1,
     title: "The First Melody",
+<<<<<<< HEAD
     note: "Every melody begins with a beautiful memory."
+=======
+    note: `Every melody begins with a beautiful memory.
+    By the way whom your looking outside window and smiling 👀👀`
+>>>>>>> 95be8bd (changes)
   },
   {
     id: 2,
     image: img2,
     song: song2,
     title: "A Beautiful Smile",
+<<<<<<< HEAD
     note: "Some smiles remain forever in our hearts."
+=======
+    note: `Some smiles remain forever in our hearts.
+    By the way, is that girl a Kathi figure?😜.saaku nakidhu👀
+    `
+>>>>>>> 95be8bd (changes)
   },
   {
     id: 3,
     image: img3,
     song: song3,
     title: "Music Speaks",
+<<<<<<< HEAD
     note: "Music says everything words cannot."
+=======
+    note: `Music says everything words cannot.
+    By the way, nice mimic of shah rukh here.I'd rate it 2/10 😜`
+>>>>>>> 95be8bd (changes)
   },
   {
     id: 4,
     image: img4,
     song: song4,
     title: "Golden Moments",
+<<<<<<< HEAD
     note: "Every moment with you shines like gold."
+=======
+    note: `Every moment with you, your melody shines like gold. 
+    By the way, you thought you looked like a good boy in this picture,
+    but who are you looking at and acting like a good boy for? 😜`
+>>>>>>> 95be8bd (changes)
   },
   {
     id: 5,
     image: img5,
     song: song5,
     title: "Moonlight Memories",
+<<<<<<< HEAD
     note: "Beautiful memories never fade."
+=======
+    note: `Beautiful memories never fade.
+    By the way, ee photo ali olle innocent hudga bayali kai itre kachoke baralla papa.
+    nange gothu niv est innocent antha 🤣.jaasthi act madbedi papa ella
+    nija ankothare.`
+>>>>>>> 95be8bd (changes)
   },
   {
     id: 6,
     image: img6,
     song: song6,
     title: "Harmony",
+<<<<<<< HEAD
     note: "Life becomes beautiful with harmony."
+=======
+    note: `Life becomes beautiful with harmony..
+    By the way, nicee pose.No comments👀.Nange ee shirt ista aithu
+    nange correct aguthe ansuthe👀.
+    `
+>>>>>>> 95be8bd (changes)
   },
   {
     id: 7,
     image: img7,
     song: song7,
     title: "Dreams",
+<<<<<<< HEAD
     note: "May every dream become a melody."
+=======
+    note: `May every dream become a melody.
+    Pose eno chenagidhe but nim shirt maasth idhe.nange kotbidi😜.
+    aakade yaarna nodtha idira.hudgi chenagidala, Idio t 👀`
+>>>>>>> 95be8bd (changes)
   },
   {
     id: 8,
     image: img8,
     song: song8,
     title: "Forever Friends",
+<<<<<<< HEAD
     note: "Some friendships are timeless."
+=======
+    note: `Some friendships are timeless.
+    By the way, pose egidhe andre you are going to pick a stone🤣.
+    adhru chenagidhe😜.shirt maasth idhe👀`
+>>>>>>> 95be8bd (changes)
   },
   {
     id: 9,
     image: img9,
     song: song9,
     title: "Special Day",
+<<<<<<< HEAD
     note: "This day deserves a beautiful song."
+=======
+    note: `This day deserves a beautiful song.
+    swalpa mundhe nodi sir naavu idivi , aaglindha bari aa side nodthidara👀.`
+>>>>>>> 95be8bd (changes)
   },
   {
     id: 10,
     image: img10,
     song: song10,
     title: "The Final Encore",
+<<<<<<< HEAD
     note: "May your life always play beautiful music."
+=======
+    note: `May your life always play beautiful music.
+    At last, Hope you liked my small gift.Jokes apart,
+    Many more happy returns of the day.May your life be filled 
+    with all the happpiness in the world .
+    And let's live together for 70 years,
+    healthy and happily ,if possible 100.
+     let's explore the world and have fun together.No Kathi figures
+     are allowed 👀.gothaitha idiot👀.`
+>>>>>>> 95be8bd (changes)
   }
 ];
